13a5b87fba2d436903c2edd8e2a8248355ce10af
c3614614 <m.diallo2774@student.leedsbeckett.ac.uk>
2023-11-29 12:33:31 +0000

    modified migration to create products table

--- database/migrations/2023_11_23_132711_create_products_table.php
+++ database/migrations/2023_11_23_132711_create_products_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            
             $table->timestamps();
+            $table->string('artist');
+            $table->string('title');
+            $table->integer('price');
         });
     }
 
